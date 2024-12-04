198cca876a6a52a55584b6b281865f25f1c4452b
Steven Olthoff <steven@axiomdatascience.com>
2024-12-04 15:05:06 -0500

    wrap everything with NoSSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,41 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+'use client'
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
 import Header from './components/header.tsx'
 import Footer from './components/footer.tsx'
-
+import NoSsr from './components/NoSsr.tsx'
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
 
-export const metadata: Metadata = {
-  title: 'NARW Anthropogenic Injuries',
-  description: 'Tracking Right Whale Injuries and Mortality',
-}
+// export const metadata: Metadata = {
+//   title: 'NARW Anthropogenic Injuries',
+//   description: 'Tracking Right Whale Injuries and Mortality',
+// }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <NoSsr>
+          <Header />
+          {children}
+          <Footer />
+        </NoSsr>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+'use client'
+import NoSsr from './components/NoSsr.tsx'
 import React from 'react'
 import Hero from '@/components/hero.tsx'
 import HomeInfo from '@/components/homeInfo.tsx'
 
 export default function Home() {
   return (
-    <>
+    <NoSsr>
       <main>
         <Hero></Hero>
         <HomeInfo></HomeInfo>
       </main>
-    </>
+    </NoSsr>
   )
 }
