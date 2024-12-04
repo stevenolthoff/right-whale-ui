4e0c83f9a16568d03202cd29797940ce8a4cdbb4
Steven Olthoff <steven@axiomdatascience.com>
2024-12-04 15:33:04 -0500

    debug

--- src/app/token/page.tsx
+++ src/app/token/page.tsx
@@ -4,12 +4,14 @@ import { useEffect } from 'react'
 import { redirect } from 'next/navigation'
 import React from 'react'
 export default function Token() {
+  console.log('/token')
   const [token, setToken] = useLocalStorage('token', '')
   useEffect(() => {
+    console.log('token', token)
     setToken(globalThis.location.hash.split('#')[1])
     if (token) {
       redirect('/injury/injury-type-by-year')
     }
-  }, [token])
+  }, [token, setToken])
   return <></>
 }
