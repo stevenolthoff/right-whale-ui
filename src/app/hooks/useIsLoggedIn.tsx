782a2e29519f3c226472d75499db53eeb7e84846
Steven Olthoff <steven@axiomdatascience.com>
2024-12-04 14:47:22 -0500

    add use cient directive and add favicon

--- src/app/hooks/useIsLoggedIn.tsx
+++ src/app/hooks/useIsLoggedIn.tsx
@@ -1,12 +1,13 @@
+'use client'
 import { useState, useEffect } from 'react'
 import { useLocalStorage } from '@uidotdev/usehooks'
 
 const useIsLoggedIn = () => {
   const [token] = useLocalStorage('token')
-  const [isLoggedIn, setIsLoggedIn] = useState(!!token && (token !== ''))
+  const [isLoggedIn, setIsLoggedIn] = useState(!!token && token !== '')
 
   useEffect(() => {
-    setIsLoggedIn(!!token && (token !== ''))
+    setIsLoggedIn(!!token && token !== '')
   }, [token])
 
   return isLoggedIn
