6aa22a3e96be329260e935bdcd8debe163241382
Steven Olthoff <steven@axiomdatascience.com>
2024-12-26 20:42:48 -0500

    add boilerplate

---
+++ src/app/monitoring/custom/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Custom Page</h1>
+      <p>This is a boilerplate React component.</p>
+    </div>
+  )
+}
+
+export default CustomPage
