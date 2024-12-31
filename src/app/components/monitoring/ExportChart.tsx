590c6324dfe20b9edf4271385cc751d6d5ebc84b
Steven Olthoff <steven@axiomdatascience.com>
2024-12-31 14:36:43 -0500

    add export button to mortality total

---
+++ src/app/components/monitoring/ExportChart.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import * as htmlToImage from 'html-to-image'
+
+interface ExportChartProps {
+  chartRef: React.RefObject<HTMLDivElement>
+  filename: string
+}
+
+export const ExportChart: React.FC<ExportChartProps> = ({ chartRef, filename }) => {
+  const handleExport = async () => {
+    if (!chartRef.current) return
+    
+    try {
+      const dataUrl = await htmlToImage.toPng(chartRef.current, {
+        quality: 1.0,
+        backgroundColor: 'white',
+      })
+      
+      const link = document.createElement('a')
+      link.download = filename
+      link.href = dataUrl
+      link.click()
+    } catch (error) {
+      console.error('Error exporting chart:', error)
+    }
+  }
+
+  return (
+    <button
+      onClick={handleExport}
+      className="ml-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors flex items-center gap-2"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+      </svg>
+      Export Chart
+    </button>
+  )
+} 
--- src/app/public-charts/mortality-total/page.tsx
+++ src/app/public-charts/mortality-total/page.tsx
@@ -1,13 +1,14 @@
 'use client'
-import React from 'react'
+import React, { useRef } from 'react'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 import { useMortalityData } from '@/app/hooks/useMortalityData'
 import { YearRangeSlider } from '@/app/components/monitoring/YearRangeSlider'
 import { useMortalityYearRange } from '@/app/hooks/useMortalityYearRange'
-import ChartLayout from '@/app/components/chartLayout'
 import { Loader } from '@/app/components/ui/Loader'
+import { ExportChart } from '@/app/components/monitoring/ExportChart'
 
 export default function MortalityTotal() {
+  const chartRef = useRef<HTMLDivElement>(null)
   const { data, loading, error } = useMortalityData()
   const { yearRange, setYearRange, minYear, maxYear } = useMortalityYearRange(data)
 
@@ -40,35 +41,47 @@ export default function MortalityTotal() {
 
   return (
     <div className='flex flex-col space-y-4 bg-white p-4'>
-      <YearRangeSlider
-        yearRange={yearRange}
-        minYear={minYear}
-        maxYear={maxYear}
-        onChange={setYearRange}
-      />
+      <div className="flex justify-between items-center">
+        <div className="flex-grow">
+          <YearRangeSlider
+            yearRange={yearRange}
+            minYear={minYear}
+            maxYear={maxYear}
+            onChange={setYearRange}
+          />
+        </div>
+        <ExportChart 
+          chartRef={chartRef}
+          filename={`mortality-total-${yearRange[0]}-${yearRange[1]}.png`}
+        />
+      </div>
       
-      <div className='h-[500px] w-full'>
+      <div ref={chartRef} className='h-[500px] w-full'>
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             data={chartData}
             margin={{
               top: 20,
               right: 30,
-              left: 20,
-              bottom: 5,
+              left: 40,
+              bottom: 35,
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis
               dataKey="year"
-              label={{ value: 'Year', position: 'insideBottom', offset: -5 }}
+              label={{ 
+                value: 'Year', 
+                position: 'insideBottom', 
+                offset: -15
+              }}
             />
             <YAxis
               label={{
                 value: 'Number of Mortalities',
                 angle: -90,
                 position: 'insideLeft',
-                offset: 10,
+                offset: 15,
               }}
             />
             <Tooltip
@@ -79,7 +92,6 @@ export default function MortalityTotal() {
               dataKey="count"
               fill="#1d4ed8"
               name="Mortalities"
-              radius={[4, 4, 0, 0]}
             />
           </BarChart>
         </ResponsiveContainer>
