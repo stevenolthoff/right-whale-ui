1f56833457a4db25ff9e45e12a12b91be61f3f55
Steven Olthoff <steven@axiomdatascience.com>
2025-01-03 15:57:31 -0500

    add /test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+export default function TestPage() {
+  return (
+    <main className='min-h-screen bg-gray-50 pt-28 pb-16 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='text-center mb-12'>
+          <h1 className='text-4xl font-bold text-gray-900 mb-4'>Test Page</h1>
+          <p className='text-lg text-gray-600'>
+            If you can see this page, the deployment was successful!
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
