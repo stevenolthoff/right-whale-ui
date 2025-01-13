c2a2cd1f140bcd342f0de2a2325929a475240e9f
Steven Olthoff <steven@axiomdatascience.com>
2025-01-13 16:08:15 -0500

    fix client side break

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,13 @@ import './globals.css'
 import Header from './components/header'
 import Footer from './components/footer'
 import NoSsr from './components/NoSsr'
-import FloatingInfoButton from './components/FloatingInfoButton'
-import DisclaimerPopup from './components/DisclaimerPopup'
 import React, { useState, useEffect } from 'react'
-import { useLocalStorage } from '@uidotdev/usehooks'
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const [hasVisited, setHasVisited] = useLocalStorage('hasVisitedBefore', false)
-  const [isPopupOpen, setIsPopupOpen] = useState(false)
-
-  useEffect(() => {
-    if (!hasVisited) {
-      setIsPopupOpen(true)
-      setHasVisited(true)
-    }
-  }, [hasVisited, setHasVisited])
-
   return (
     <html lang='en'>
       <body
@@ -32,11 +19,6 @@ export default function RootLayout({
           <Header />
           <div className='flex-grow'>{children}</div>
           <Footer />
-          <FloatingInfoButton onClick={() => setIsPopupOpen(true)} />
-          <DisclaimerPopup
-            open={isPopupOpen}
-            onClose={() => setIsPopupOpen(false)}
-          />
         </NoSsr>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 'use client'
 import NoSsr from './components/NoSsr'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Hero from '@/components/hero'
 import HomeInfo from '@/components/homeInfo'
 import DisclaimerPopup from './components/DisclaimerPopup'
+import { useLocalStorage } from '@uidotdev/usehooks'
+import FloatingInfoButton from './components/FloatingInfoButton'
 
 export default function Home() {
+  const [hasVisited, setHasVisited] = useLocalStorage('hasVisitedBefore', false)
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+  useEffect(() => {
+    if (!hasVisited) {
+      setIsPopupOpen(true)
+      setHasVisited(true)
+    }
+  }, [hasVisited, setHasVisited])
   return (
     <NoSsr>
       <main>
         <Hero></Hero>
         <HomeInfo></HomeInfo>
+        <DisclaimerPopup
+          open={isPopupOpen}
+          onClose={() => setIsPopupOpen(false)}
+        />
+        <FloatingInfoButton onClick={() => setIsPopupOpen(true)} />
       </main>
     </NoSsr>
   )
