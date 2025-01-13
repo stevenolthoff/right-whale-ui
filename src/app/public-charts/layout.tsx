e697475b4dbeba28813d5ef4e808dbc33af1feaf
Steven Olthoff <steven@axiomdatascience.com>
2025-01-13 16:26:13 -0500

    add public charts descriptions

--- src/app/public-charts/layout.tsx
+++ src/app/public-charts/layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Sidebar from '../components/monitoring/Sidebar'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 export default function PublicChartsLayout({
   children,
@@ -9,39 +10,97 @@ export default function PublicChartsLayout({
   children: React.ReactNode
 }) {
   const pathname = usePathname()
-  const text: Record<string, { title: string; description: string }> = {
+  const text: Record<
+    string,
+    { title: string; description: string | React.ReactNode }
+  > = {
     '/public-charts/overview': {
       title: 'Introduction to the Data',
       description: '',
     },
     '/public-charts/mortality-total': {
       title: 'Mortality Total',
-      description: 'View total mortality statistics.',
+      description: (
+        <>
+          Total detected North Atlantic right whale mortalities over time.
+          Mortalities illustrated here represent those from all sources, known
+          and unknown. As detected mortalities represent a fraction of actual
+          mortalities (
+          <Link
+            href='https://conbio.onlinelibrary.wiley.com/doi/full/10.1111/csp2.346'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 hover:text-blue-700'
+          >
+            Pace et al 2017
+          </Link>
+          ) these counts are a minimum and likely underestimates.
+        </>
+      ),
     },
     '/public-charts/mortality-by-country': {
       title: 'Mortality by Country',
-      description: 'View mortality statistics broken down by country.',
+      description:
+        'Detected North Atlantic right whale mortalities by country in which the mortality was documented. Detection location is not necessarily indicative of where a whale was injured or where it died.',
     },
     '/public-charts/mortality-by-cause-of-death': {
       title: 'Mortality by Cause',
-      description: 'View mortality statistics categorized by cause of death.',
+      description: (
+        <>
+          Cause of death for detected North Atlantic right whale mortalities.
+          See{' '}
+          <Link
+            href='https://www.int-res.com/abstracts/dao/v135/n1/p1-31/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 hover:text-blue-700'
+          >
+            Sharp et al 2019
+          </Link>{' '}
+          for more information. Cause of death cannot be determined for all
+          cases due to carcass inaccessibility, decomposition, and other
+          factors. Those mortalities for which a cause of death could not be
+          identified are listed as “Unknown.” Mortalities listed as “Other”
+          include a fetus that washed ashore, shark predation, and a hunted
+          animal.
+        </>
+      ),
     },
     '/public-charts/mortality-by-cause-of-death-and-country': {
       title: 'Mortality by Cause and Country',
       description:
-        'View mortality statistics broken down by both cause and country.',
+        'Cause of death and detection location for North Atlantic right whale mortalities. Cause of death cannot be determined for all cases due to carcass inaccessibility, decomposition, and other factors. Those mortalities for which a cause of death could not be identified are listed as “Unknown.” Mortalities listed as “Other” include fetuses that washed ashore, shark predation, and a hunted animal. Detection location is not necessarily indicative of where a whale was injured or where it died.',
     },
     '/public-charts/calving': {
       title: 'Calving',
-      description: 'View calving statistics.',
+      description:
+        'North Atlantic right whale calving events by year. These data are updated at the end of the calving season which runs December – March annually. ',
     },
     '/public-charts/injury-total': {
       title: 'Injury Total',
-      description: 'View total injury statistics.',
+      description:
+        'North Atlantic right whale anthropogenic injuries detected by year. These data include anthropogenic injuries only and do not include those for which a source could not be identified. These data are assessed on an annual basis once all sighting and photographic information have been received. As such, there is generally a two-year lag in data visualization.',
     },
     '/public-charts/entanglement': {
       title: 'Entanglement',
-      description: 'View entanglement-related statistics.',
+      description: (
+        <>
+          Detected entanglement events by type and severity. Entanglements can
+          be detected with attached gear (i.e. Gear) or by wounds only and no
+          attached gear (i.e. No Gear). Severity is an assessment of the
+          resulting entanglement injury and is classified as Minor, Moderate, or
+          Severe. See{' '}
+          <Link
+            href='https://conbio.onlinelibrary.wiley.com/doi/full/10.1111/cobi.12590'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 hover:text-blue-700'
+          >
+            Knowlton et al 2016
+          </Link>{' '}
+          for severity descriptions.
+        </>
+      ),
     },
   }
 
