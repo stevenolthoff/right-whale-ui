f7a46695ccb5cc109cba713392ae62baab6acf95
Steven <steven@axds.co>
2025-08-05 12:53:13 -0700

    Finalizations

--- src/app/hooks/useFilteredData.ts
+++ src/app/hooks/useFilteredData.ts
@@ -1,18 +1,22 @@
 'use client'
 import { create } from 'zustand'
 import { InjuryCase } from '../types/monitoring'
-import { ColumnDef } from '@tanstack/react-table'
+import { ColumnDef, ColumnFiltersState } from '@tanstack/react-table'
 
 interface FilteredDataStore {
   filteredData: InjuryCase[]
   columns: ColumnDef<InjuryCase, any>[]
+  columnFilters: ColumnFiltersState
   setFilteredData: (data: InjuryCase[]) => void
   setColumns: (columns: ColumnDef<InjuryCase, any>[]) => void
+  setColumnFilters: (filters: ColumnFiltersState) => void
 }
 
 export const useFilteredData = create<FilteredDataStore>((set) => ({
   filteredData: [],
   columns: [],
+  columnFilters: [],
   setFilteredData: (data) => set({ filteredData: data }),
   setColumns: (columns) => set({ columns }),
+  setColumnFilters: (filters) => set({ columnFilters: filters }),
 })) 
